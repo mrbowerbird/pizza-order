10615331d8dce7f08a4433f4c8f45e1556708f9d
Michael <mbutcherus@gmail.com>
2018-10-26 12:56:03 -0700

    Create way to count toppings, to be used in calculations later

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
   $("form#pizza_order").submit(function(event){
     event.preventDefault();
     $("#order-recap").show();
+    var checkboxes = $("input:checkbox[name=topping]:checked");
+    var countToppings = checkboxes.filter(':checked').length;
     $("input:checkbox[name=topping]:checked").each(function(){
       var yourToppings = $(this).val();
+      console.log(countToppings);
       $('#order-recap').append(yourToppings + "<br>");
     });
     $('#pizza_order').hide();
